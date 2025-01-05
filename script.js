db08650c2276a9a7efcec9f5ade320d362982c51
lukazko <lukaz.kubik@gmail.com>
2025-01-05 17:51:29 +0100

    standardized apostrophes

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
      * @returns {string[]} An array of words from the cleaned text.
      */
     function cleanAndSplit(text) {
-        return text.replace(/[.,!?;:]/g, '').toLowerCase().split(/\s+/); // Remove punctuation, convert to lowercase, and split by whitespace.
+        return text.replace(/[.,!?;:]/g, '').replace('\'','’').toLowerCase().split(/\s+/); // Remove punctuation, standard apostrophes, convert to lowercase, and split by whitespace.
     }
 
     /**
